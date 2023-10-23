6f8da4eb1743c32b7fb36b624d680b1a52f94073
RomanZemlin <roman.zemlin@mail.ru>
2023-10-24 00:09:55 +0300

    c_w

--- email_distribution/scheduler.py
+++ email_distribution/scheduler.py
@@ -1,12 +1,32 @@
+import threading
+import time
+from datetime import datetime
+
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import DjangoJobStore
 
+from email_distribution.models import EmailDistribution
 from email_distribution.services import send_email
 
 scheduler = BackgroundScheduler()
 scheduler.add_jobstore(DjangoJobStore(), "default")
 
 
-@scheduler.scheduled_job('interval', days=1)
 def schedule_email():
     send_email()
+
+
+def check_scheduler():
+    while True:
+        # Проверка состояния планировщика
+        if scheduler.state == 'running':
+            # Если активен, спать на 60 минут
+            time.sleep(3600)
+        else:
+            # Если неактивен, включить планировщик
+            scheduler.start()
+            # Здесь можно добавить дополнительную логику, например, получение новых значений из формы или проверка условий для активации планировщика
+
+
+# Запуск отдельного потока для проверки состояния планировщика
+check_thread = threading.Thread(target=check_scheduler)
